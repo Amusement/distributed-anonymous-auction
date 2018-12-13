ac2e5bfce99c4e8442d1c4ecc1121d38534a7447
Denis Trailin <denis.trailin@gmail.com>
2018-12-13 15:53:38 -0800

    streaming output

--- scripts/start.py
+++ scripts/start.py
@@ -11,12 +11,10 @@ result = subprocess.run([(
 publicIPs = result.stdout.decode("utf-8").split("\n")[0:-1]
 
 
-
 class SSHThread(threading.Thread):
     def __init__(self, ip):
         super(SSHThread, self).__init__()
         self.ip = ip
-
 
     def run(self):
         ssh = paramiko.SSHClient()
@@ -26,17 +24,10 @@ class SSHThread(threading.Thread):
         ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(
             "sudo go run P2-d3w9a-b3c0b-b3l0b-k0b9/auctioneer_main.go P2-d3w9a-b3c0b-b3l0b-k0b9/auctioneer/config.json")
 
-        def line_buffered(f):
-            line_buf = ""
-            while not f.channel.exit_status_ready():
-                line_buf += f.read(1)
-                if line_buf.endswith('\n'):
-                    yield line_buf
-                    line_buf = ''
+        ssh_stdin.close()
+        for line in iter(lambda: ssh_stdout.readline(2048), ""):
+            print(line, end="")
 
-        for l in line_buffered(ssh_stdout):
-            print(self.ip+": "+ l)
-            
         ssh.close()
 
 
@@ -46,7 +37,5 @@ for ip in publicIPs[1:]:
     threads.append(ssh_thread)
     ssh_thread.start()
 
-
 for thread in threads:
-   thread.join()
-
+    thread.join()
