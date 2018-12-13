1df4a01d7e5a2e6a7cbcfc4912db84f463bc63ab
b3l0b <b3l0b@ugrad.cs.ubc.ca>
2018-12-13 14:19:54 -0800

    script pull

--- scripts/pull.py
+++ scripts/pull.py
@@ -6,7 +6,7 @@ import json
 from constants import *
 
 def pull(ssh):
-    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(("cd P2-d3w9a-b3c0b-b3l0b-k0b9$; git stash; git pull; git stash pop"))
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("cd P2-d3w9a-b3c0b-b3l0b-k0b9$; git stash; git pull; git stash pop")
     ssh_stdout.channel.recv_exit_status()
     lines = ssh_stdout.readlines()
     for line in lines:
@@ -17,9 +17,7 @@ def pull(ssh):
         print(line)
 
 
-createSeller(publicIPs[0])
-
-for ip in publicIPs[1:]:
+for ip in publicIPs:
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(ip, username=VMusername, password=VMpassword)
@@ -29,3 +27,4 @@ for ip in publicIPs[1:]:
 
 print("seller " + publicIPs[0])
 print("auctioneers " + str(publicIPs[1:]))
+
